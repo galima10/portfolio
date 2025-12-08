3f708e8af7fd0d0dfdcbed961ab05a8b6089a315
Magali MAI <maimagali26.pro@gmail.com>
2025-12-08 10:55:17 +0100

    Configure vite for react ts and sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    sourcemap: false,
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+      format: {
+        comments: false,
+      },
+    },
+    rollupOptions: {
+      input: path.resolve(__dirname, "src/main.tsx"),
+    },
+    outDir: path.resolve(__dirname, "/dist"),
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@hook": path.resolve(__dirname, "src/hooks"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@types": path.resolve(__dirname, "src/types"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@store": path.resolve(__dirname, "src/store"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+    },
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".scss"],
+  },
+  define: {
+    "process.env": {},
+  },
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+  },
+});
