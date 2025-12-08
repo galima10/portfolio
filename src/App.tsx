621879789f474baee246244b4f627eeb7b275f67
Magali MAI <maimagali26.pro@gmail.com>
2025-12-08 12:13:21 +0100

    Apply fonts

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <AppButton />
+      <h1>Titre</h1>
     </>
   );
 }
--- src/components/atoms/AppButton/index.tsx
+++ src/components/atoms/AppButton/index.tsx
@@ -3,7 +3,7 @@ import styles from "./AppButton.module.scss";
 export default function AppButton() {
   return (
     <a href="/" className={styles.appButton}>
-      <p>Bouton</p>
+      Bouton
     </a>
   );
 }
