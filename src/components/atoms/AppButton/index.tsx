a28ed3d43ce1f57e8c98b5332b91715adc8ef412
Magali MAI <maimagali26.pro@gmail.com>
2025-12-08 11:37:21 +0100

    Create AppButton component and global declarations for ts

---
+++ src/components/atoms/AppButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./AppButton.module.scss";
+
+export default function AppButton() {
+  return (
+    <a href="#" className={styles.appButton}>
+      <p>Bouton</p>
+    </a>
+  );
+}
