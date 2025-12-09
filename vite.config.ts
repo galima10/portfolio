f011d5bda796d9cd508bd6585eaf1a613792f4af
Magali MAI <maimagali26.pro@gmail.com>
2025-12-09 10:56:00 +0100

    Change build config base and workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio-site/",
+  base: "./",
   plugins: [react()],
   build: {
     sourcemap: false,
