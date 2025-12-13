e0bff599b152345f8053404c1430f10a17c78d7d
Magali MAI <maimagali26.pro@gmail.com>
2025-12-13 22:57:25 +0100

    Add dynamic display for projects

--- src/components/organisms/Projects/index.tsx
+++ src/components/organisms/Projects/index.tsx
@@ -2,7 +2,7 @@ import styles from "./Projects.module.scss";
 
 import SectionWrapper from "@components/wrappers/SectionWrapper";
 
-// import { projects } from "@constants/projects";
+import { projects } from "@constants/projects";
 
 import ProjectItem from "@components/molecules/ProjectItem";
 
@@ -15,36 +15,17 @@ export default function Projects() {
       className={styles.projectsSection}
     >
       <ul className={styles.projectsList}>
-        <li>
-          <ProjectItem
-            project={{
-              id: "Projet 1",
-              name: "Meam¡",
-              island: "island1",
-              hook: "Une aventure pas comme les autres, où je trace seule le chemin d’un projet qui m’appartient.",
-              label: "Personnel — Dév. web — Appli. mobile",
-              stack: [
-                {
-                  label: "TypeScript",
-                  icon: "typescript",
-                },
-                {
-                  label: "React Native",
-                  icon: "react",
-                },
-                {
-                  label: "Expo",
-                  icon: "expo",
-                },
-                {
-                  label: "Jest",
-                  icon: "jest",
-                },
-              ],
-              description: "Description du projet Meam¡...",
-            }}
-          />
-        </li>
+        {projects.map((project, index) => {
+          return (
+            <li key={project.id} className={styles.projectItem}>
+              <ProjectItem
+                project={project}
+                orientation={index % 2 === 0 ? "left" : "right"}
+                
+              />
+            </li>
+          );
+        })}
       </ul>
     </SectionWrapper>
   );
