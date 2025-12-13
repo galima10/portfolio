1bb16005ee99b39f55ad2422261dbba35d78b6ca
Magali MAI <maimagali26.pro@gmail.com>
2025-12-13 19:13:11 +0100

    Add map disposition with responsive design

--- src/components/organisms/Technos/index.tsx
+++ src/components/organisms/Technos/index.tsx
@@ -3,6 +3,7 @@ import styles from "./Technos.module.scss";
 import SectionWrapper from "@components/wrappers/SectionWrapper";
 
 import AppSVG from "@components/atoms/svgIcons/AppSVG";
+import GenericButton from "@components/atoms/appElements/GenericButton";
 
 export default function Technos() {
   return (
@@ -10,11 +11,26 @@ export default function Technos() {
       title="Mes territoires"
       orientation="right"
       className={styles.technos}
+      id="technos"
     >
-      <AppSVG name="languageMap" className={styles.languageMap} />
-      <AppSVG name="frameworksMap" className={styles.frameworksMap} />
-      <AppSVG name="toolsMap" className={styles.toolsMap} />
-      <AppSVG name="discussedMap" className={styles.discussedMap} />
+      <div className={styles.technoMaps}>
+        <GenericButton className={styles.technoItem + " " + styles.languageMap}>
+          <AppSVG name="languageMap" className={styles.map} />
+          <h3>Langages maîtrisés</h3>
+        </GenericButton>
+        <GenericButton className={styles.technoItem + " " + styles.frameworksMap}>
+          <AppSVG name="frameworksMap" className={styles.map} />
+          <h3>Frameworks & Libraries</h3>
+        </GenericButton>
+        <GenericButton className={styles.technoItem + " " + styles.toolsMap}>
+          <AppSVG name="toolsMap" className={styles.map} />
+          <h3>Outils & Logiciels</h3>
+        </GenericButton>
+        <GenericButton className={styles.technoItem + " " + styles.discussedMap}>
+          <AppSVG name="discussedMap" className={styles.map} />
+          <h3>Abordés</h3>
+        </GenericButton>
+      </div>
     </SectionWrapper>
   );
 }
