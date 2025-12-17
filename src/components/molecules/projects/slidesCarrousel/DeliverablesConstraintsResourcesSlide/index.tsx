8ce5907da7e7a125d8defa93561bc305527d6bb7
Magali MAI <maimagali26.pro@gmail.com>
2025-12-17 08:59:30 +0100

    WIP deliverables, contraints and resources slide

--- src/components/molecules/projects/slidesCarrousel/DeliverablesConstraintsResourcesSlide/index.tsx
+++ src/components/molecules/projects/slidesCarrousel/DeliverablesConstraintsResourcesSlide/index.tsx
@@ -1,7 +1,28 @@
 import styles from "../Carrousel.module.scss";
 
-export default function DeliverablesConstraintsResources(){
-    return (
-        <div></div>
-    );
-};
+interface DeliverablesConstraintsResourcesSlideProps {
+  deliverablesConstraintsResources: {
+    deliverables: {
+      name: string;
+      format: string;
+      goal: string;
+      specificities?: string;
+      technologies: string;
+    }[];
+    constraints: {
+      temporal?: string[];
+      technical?: string[];
+      budget?: string[];
+      graphic?: string[];
+      geographic?: string[];
+    };
+    resources: {
+      available: string[];
+      missing: string[];
+    };
+  };
+}
+
+export default function DeliverablesConstraintsResources({ deliverablesConstraintsResources }: DeliverablesConstraintsResourcesSlideProps) {
+  return <div className={styles.deliverablesConstraintsResourcesSlide}></div>;
+}
