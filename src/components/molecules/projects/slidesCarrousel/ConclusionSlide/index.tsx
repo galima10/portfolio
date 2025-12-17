36f874b90357f7c855715c72ca48dee70df37410
Magali MAI <maimagali26.pro@gmail.com>
2025-12-17 23:40:01 +0100

    Stylize conclusion slide

--- src/components/molecules/projects/slidesCarrousel/ConclusionSlide/index.tsx
+++ src/components/molecules/projects/slidesCarrousel/ConclusionSlide/index.tsx
@@ -1,11 +1,20 @@
 import styles from "../Carrousel.module.scss";
 
+import { projectTitleDesc } from "@constants/global";
+
 interface ConclusionSlideProps {
   conclusion: string;
 }
 
 export default function ConclusionSlide({ conclusion }: ConclusionSlideProps) {
-  return <div className={`${styles.slides} ${styles.conclusionSlide}`}>
-    <p>{conclusion}</p>
-  </div>;
+  return (
+    <div className={styles.slides}>
+      <h2 className="project-h2">{projectTitleDesc["conclusion"]}</h2>
+      <div className={`${styles.slideContent} ${styles.conclusionSlide}`}>
+        {conclusion.split(". ").map((sentence, index) => (
+          <p key={index}>{sentence}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
