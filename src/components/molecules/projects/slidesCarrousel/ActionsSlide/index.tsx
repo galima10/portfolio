a131d3637884d541af8b4d54bb020dfbe96fab9b
Magali MAI <maimagali26.pro@gmail.com>
2025-12-17 23:33:18 +0100

    Stylize action slide

--- src/components/molecules/projects/slidesCarrousel/ActionsSlide/index.tsx
+++ src/components/molecules/projects/slidesCarrousel/ActionsSlide/index.tsx
@@ -1,5 +1,8 @@
 import styles from "../Carrousel.module.scss";
 
+import GenericButton from "@components/atoms/appElements/GenericButton";
+import { translationsProjects, projectTitleDesc } from "@constants/global";
+
 interface ActionsSlideProps {
   actions: {
     missions: string[];
@@ -10,30 +13,42 @@ interface ActionsSlideProps {
 
 export default function ActionsSlide({ actions }: ActionsSlideProps) {
   return (
-    <div className={`${styles.slides} ${styles.actionsSlide}`}>
-      <div>
-        <strong>Missions</strong>
-        <ul>
-          {actions.missions.map((mission, index) => (
-            <li key={`${mission}-${index}`}>{mission}</li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <strong>Tools</strong>
-        <ul>
-          {actions.tools.map((tool, index) => (
-            <li key={`${tool}-${index}`}>{tool}</li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <strong>Skills</strong>
-        <ul>
-          {actions.skills.map((skill, index) => (
-            <li key={`${skill}-${index}`}>{skill}</li>
-          ))}
-        </ul>
+    <div className={styles.slides}>
+      <h2 className="project-h2">{projectTitleDesc["actions"]}</h2>
+      <div className={`${styles.slideContent} ${styles.actionsSlide}`}>
+        <div className={styles.buttonItem}>
+          <GenericButton className={styles.button}>
+            <span>{translationsProjects["missions"]}</span>
+            <span>▶</span>
+          </GenericButton>
+          <ul>
+            {actions.missions.map((mission, index) => (
+              <li key={`${mission}-${index}`}>{mission}</li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.buttonItem}>
+          <GenericButton className={styles.button}>
+            <span>{translationsProjects["tools"]}</span>
+            <span>▶</span>
+          </GenericButton>
+          <ul>
+            {actions.tools.map((tool, index) => (
+              <li key={`${tool}-${index}`}>{tool}</li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.buttonItem}>
+          <GenericButton className={styles.button}>
+            <span>{translationsProjects["skills"]}</span>
+            <span>▶</span>
+          </GenericButton>
+          <ul>
+            {actions.skills.map((skill, index) => (
+              <li key={`${skill}-${index}`}>{skill}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/constants/global/index.ts
+++ src/constants/global/index.ts
@@ -60,5 +60,8 @@ export const translationsProjects = {
   geographic: "Géographiques",
   available: "Disponibles",
   missing: "Manquantes",
+  missions: "Missions",
+  tools: "Outils",
+  skills: "Compétences",
 }
 
