34c010369adcf7370ea83153b5960b7950aa86b4
Magali MAI <maimagali26.pro@gmail.com>
2025-12-21 10:09:03 +0100

    Add a zoom dezoom smooth

--- src/components/molecules/main/TechnosMap/index.tsx
+++ src/components/molecules/main/TechnosMap/index.tsx
@@ -8,49 +8,54 @@ import { useState } from "react";
 export default function TechnosMap() {
   const [selectedTechno, setSelectedTechno] = useState<string | null>(null);
   return (
-    <div
-      className={
-        styles.technoMaps +
-        (selectedTechno ? ` ${styles[selectedTechno + "Centred"]}` : "")
-      }
-    >
-      <TechnoMapItem
-        techno={{
-          name: "Langages maîtrisés",
-          slug: "languagesMap",
-          className: "languagesMap",
-        }}
-        setSelectedTechno={setSelectedTechno}
-        selectedTechno={selectedTechno}
-      />
-      <TechnoMapItem
-        techno={{
-          name: "Frameworks & Libraries",
-          slug: "frameworksMap",
-          className: "frameworksMap",
-        }}
-        setSelectedTechno={setSelectedTechno}
-        selectedTechno={selectedTechno}
-      />
-      <TechnoMapItem
-        techno={{
-          name: "Outils & Logiciels",
-          slug: "toolsMap",
-          className: "toolsMap",
-        }}
-        setSelectedTechno={setSelectedTechno}
-        selectedTechno={selectedTechno}
-      />
-      <TechnoMapItem
-        techno={{
-          name: "Abordés",
-          slug: "discussedMap",
-          className: "discussedMap",
-        }}
-        setSelectedTechno={setSelectedTechno}
-        selectedTechno={selectedTechno}
-      />
-      <GenericButton className={styles.backButton} action={() => setSelectedTechno(null)}>
+    <div className={styles.technosMapContainer}>
+      <div
+        className={
+          styles.technoMaps +
+          (selectedTechno ? ` ${styles[selectedTechno + "Centred"]}` : "")
+        }
+      >
+        <TechnoMapItem
+          techno={{
+            name: "Langages maîtrisés",
+            slug: "languagesMap",
+            className: "languagesMap",
+          }}
+          setSelectedTechno={setSelectedTechno}
+          selectedTechno={selectedTechno}
+        />
+        <TechnoMapItem
+          techno={{
+            name: "Frameworks & Libraries",
+            slug: "frameworksMap",
+            className: "frameworksMap",
+          }}
+          setSelectedTechno={setSelectedTechno}
+          selectedTechno={selectedTechno}
+        />
+        <TechnoMapItem
+          techno={{
+            name: "Outils & Logiciels",
+            slug: "toolsMap",
+            className: "toolsMap",
+          }}
+          setSelectedTechno={setSelectedTechno}
+          selectedTechno={selectedTechno}
+        />
+        <TechnoMapItem
+          techno={{
+            name: "Abordés",
+            slug: "discussedMap",
+            className: "discussedMap",
+          }}
+          setSelectedTechno={setSelectedTechno}
+          selectedTechno={selectedTechno}
+        />
+      </div>
+      <GenericButton
+        className={styles.backButton}
+        action={() => setSelectedTechno(null)}
+      >
         ↩
       </GenericButton>
     </div>
