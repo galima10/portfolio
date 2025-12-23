7a0f901363397b443f60fa712ed13001fc4fe1f4
Magali MAI <maimagali26.pro@gmail.com>
2025-12-23 11:55:23 +0100

    Create empty svg component

---
+++ src/components/atoms/appElements/AppSVGTimeLine/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./AppSVGTimeLine.module.scss";
+
+export default function AppSVGTimeLine() {
+  return <svg className={styles.timelineSvg} aria-hidden="true" />;
+}
---
+++ src/components/timeline/TimeLine/index.tsx
@@ -0,0 +1,9 @@
+import styles from './TimeLine.module.scss';
+
+export default function TimeLine(){
+    return (
+        <div className={styles.timeline}>
+
+        </div>
+    );
+};
